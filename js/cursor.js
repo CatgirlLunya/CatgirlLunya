5e9ed0a5914c1f56ead3dd95d9fb749b0b321838
Luna <catgirl.lunya@gmail.com>
2024-05-29 22:58:15 -0400

    Fix header

---
+++ js/cursor.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", (event) => {
+  new cursoreffects.rainbowCursor({
+    length: 30,
+    colors: ["#5BCEFA", "#F5A9B8", "#FFFFFF", "#F5A9B8", "#5BCEFA"],
+    size: 4,
+  });
+});
